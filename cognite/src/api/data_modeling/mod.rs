a9e0b2f8b9a977e6113abe6d9640e7f4adc1d3ef
Einar Omang <einar.omang@cognite.com>
2023-10-31 12:34:54 +0100

    Add to client

--- cognite/src/api/data_modeling/mod.rs
+++ cognite/src/api/data_modeling/mod.rs
@@ -9,12 +9,16 @@ use std::sync::Arc;
 use crate::api::data_modeling::{instances::Instances, views::Views};
 use crate::ApiClient;
 
+use self::containers::Containers;
+use self::data_models::DataModels;
 use self::spaces::Spaces;
 
 pub struct Models {
     pub instances: Instances,
     pub views: Views,
     pub spaces: Spaces,
+    pub data_models: DataModels,
+    pub containers: Containers,
 }
 
 impl Models {
@@ -22,7 +26,9 @@ impl Models {
         Models {
             instances: Instances::new(api_client.clone()),
             views: Views::new(api_client.clone()),
-            spaces: Spaces::new(api_client),
+            spaces: Spaces::new(api_client.clone()),
+            data_models: DataModels::new(api_client.clone()),
+            containers: Containers::new(api_client),
         }
     }
 }
