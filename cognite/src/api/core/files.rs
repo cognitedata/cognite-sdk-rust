f0a4510a73e46408920712d654283ef368966f25
Einar Omang <einar.omang@cognite.com>
2025-08-12 13:30:52 +0200

    Fix new warning on latest rustc
    Warning is about some elided lifetime, change does not affect
    functionality.

--- cognite/src/api/core/files.rs
+++ cognite/src/api/core/files.rs
@@ -337,7 +337,7 @@ impl Files {
     fn create_multipart_upload(
         &self,
         res: FileUploadResult<MultiUploadUrls>,
-    ) -> Result<(MultipartUploader, FileMetadata)> {
+    ) -> Result<(MultipartUploader<'_>, FileMetadata)> {
         Ok((
             MultipartUploader::new(
                 self,
