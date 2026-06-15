e0c3e9a098bd9ec9295bab80515672f4b6328f92
Everton Colling <evertoncolling@gmail.com>
2026-06-15 12:02:05 +0200

    Add tests

--- cognite/tests/datapoints_tests.rs
+++ cognite/tests/datapoints_tests.rs
@@ -3,10 +3,73 @@
 #[cfg(test)]
 use cognite::time_series::*;
 use cognite::*;
+use models::instances::{
+    CogniteExtendable, CogniteTimeseries, InstanceId, NodeAndEdgeCreateCollection,
+    NodeOrEdgeCreate, NodeOrEdgeSpecification, TimeSeriesType, Timeseries, WithInstance, WithView,
+};
+use models::ItemId;
 
 mod common;
 pub use common::*;
 use futures::TryStreamExt;
+use serde::{Deserialize, Serialize};
+use serde_with::skip_serializing_none;
+
+#[skip_serializing_none]
+#[derive(Serialize, Deserialize, Clone, Debug, Default)]
+#[serde(rename_all = "camelCase")]
+struct StateSetState {
+    numeric_value: i32,
+    string_value: String,
+}
+
+#[skip_serializing_none]
+#[derive(Serialize, Deserialize, Clone, Debug, Default)]
+#[serde(rename_all = "camelCase")]
+struct CogniteStateSetProperties {
+    name: Option<String>,
+    states: Vec<StateSetState>,
+}
+
+impl WithView for CogniteStateSetProperties {
+    const SPACE: &'static str = "cdf_cdm";
+    const EXTERNAL_ID: &'static str = "CogniteStateSet";
+    const VERSION: &'static str = "v1";
+}
+
+type CogniteStateSet = CogniteExtendable<CogniteStateSetProperties>;
+
+async fn beta_upsert_instances<TProperties>(
+    client: &CogniteClient,
+    items: Vec<NodeOrEdgeCreate<TProperties>>,
+) -> cognite::Result<()>
+where
+    TProperties: Serialize + Send + Sync,
+{
+    let collection = NodeAndEdgeCreateCollection {
+        items,
+        auto_create_direct_relations: Some(true),
+        ..Default::default()
+    };
+    client
+        .api_client
+        .clone_with_api_version("beta")
+        .post::<::serde_json::Value, _>("models/instances", &collection)
+        .await?;
+    Ok(())
+}
+
+async fn beta_delete_instances(
+    client: &CogniteClient,
+    items: &[NodeOrEdgeSpecification],
+) -> cognite::Result<()> {
+    client
+        .api_client
+        .clone_with_api_version("beta")
+        .post::<::serde_json::Value, _>("models/instances/delete", &Items::new(items))
+        .await?;
+    Ok(())
+}
 
 async fn create_test_ts(client: &CogniteClient, is_string: bool, idx: i32) -> TimeSeries {
     let ts = AddTimeSeries {
@@ -203,6 +266,175 @@ async fn create_retrieve_delete_string_datapoints() {
 }
 
 #[tokio::test]
+async fn create_retrieve_latest_delete_state_datapoints() {
+    let space = std::env::var("CORE_DM_TEST_SPACE")
+        .expect("CORE_DM_TEST_SPACE must be set to a writable data modeling space");
+    let state_set_external_id = format!("{}-state-set", uuid::Uuid::new_v4());
+    let ts_external_id = format!("{}-state-ts", uuid::Uuid::new_v4());
+    let client = get_client();
+
+    let state_set = CogniteStateSet::new(
+        space.clone(),
+        state_set_external_id.clone(),
+        CogniteStateSetProperties {
+            name: Some("Rust SDK state datapoint test states".to_string()),
+            states: vec![
+                StateSetState {
+                    numeric_value: 0,
+                    string_value: "OFF".to_string(),
+                },
+                StateSetState {
+                    numeric_value: 1,
+                    string_value: "RUNNING".to_string(),
+                },
+                StateSetState {
+                    numeric_value: 2,
+                    string_value: "STANDBY".to_string(),
+                },
+            ],
+            ..Default::default()
+        },
+    );
+    beta_upsert_instances(&client, vec![state_set.instance()])
+        .await
+        .unwrap();
+
+    let mut timeseries =
+        CogniteTimeseries::new(space.clone(), ts_external_id.clone(), Timeseries::new(true));
+    timeseries.properties.r#type = TimeSeriesType::State;
+    timeseries.properties.state_set = Some(InstanceId {
+        space: space.clone(),
+        external_id: state_set_external_id.clone(),
+    });
+    beta_upsert_instances(&client, vec![timeseries.instance()])
+        .await
+        .unwrap();
+
+    let ts_instance_id = InstanceId {
+        space: space.clone(),
+        external_id: ts_external_id.clone(),
+    };
+    let start = 1_735_689_600_000;
+
+    client
+        .time_series
+        .insert_datapoints(vec![AddDatapoints {
+            id: ts_instance_id.clone().into(),
+            datapoints: DatapointsEnumType::StateDatapoints(vec![
+                DatapointState {
+                    timestamp: start,
+                    numeric_value: Some(0),
+                    string_value: Some("OFF".to_string()),
+                    status: None,
+                },
+                DatapointState {
+                    timestamp: start + 3_600_000,
+                    numeric_value: Some(1),
+                    string_value: Some("RUNNING".to_string()),
+                    status: None,
+                },
+                DatapointState {
+                    timestamp: start + 7_200_000,
+                    numeric_value: Some(2),
+                    string_value: Some("STANDBY".to_string()),
+                    status: None,
+                },
+            ]),
+        }])
+        .await
+        .unwrap();
+
+    let raw = client
+        .time_series
+        .retrieve_datapoints(&DatapointsFilter {
+            items: vec![DatapointsQuery {
+                id: ts_instance_id.clone().into(),
+                limit: Some(10),
+                ..Default::default()
+            }],
+            start: Some(start.into()),
+            end: Some((start + 10_800_000).into()),
+            ..Default::default()
+        })
+        .await
+        .unwrap();
+
+    let raw_response = raw.into_iter().next().unwrap();
+    assert_eq!(Some(CoreTimeSeriesType::State), raw_response.r#type);
+    assert_eq!(Some(ts_instance_id.clone()), raw_response.instance_id);
+    let raw_states = raw_response.datapoints.state().unwrap();
+    assert_eq!(3, raw_states.len());
+    assert_eq!(Some(0), raw_states[0].numeric_value);
+    assert_eq!(Some("OFF".to_string()), raw_states[0].string_value);
+    assert_eq!(Some(1), raw_states[1].numeric_value);
+    assert_eq!(Some("RUNNING".to_string()), raw_states[1].string_value);
+
+    let latest = client
+        .time_series
+        .retrieve_latest_datapoints(
+            &[LatestDatapointsQuery {
+                id: ts_instance_id.clone().into(),
+                before: Some((start + 10_800_000).to_string()),
+                ..Default::default()
+            }],
+            false,
+        )
+        .await
+        .unwrap();
+    assert_eq!(Some(CoreTimeSeriesType::State), latest[0].r#type);
+    assert_eq!(Some(ts_instance_id.clone()), latest[0].instance_id);
+    let latest_state = latest[0].datapoint.as_ref().unwrap().state().unwrap();
+    assert_eq!(Some(2), latest_state.numeric_value);
+    assert_eq!(Some("STANDBY".to_string()), latest_state.string_value);
+
+    client
+        .time_series
+        .delete_datapoints(&[DeleteDatapointsQuery::new(
+            ts_instance_id.clone(),
+            start,
+            start + 10_800_000,
+        )])
+        .await
+        .unwrap();
+
+    let raw_after_delete = client
+        .time_series
+        .retrieve_datapoints(&DatapointsFilter {
+            items: vec![DatapointsQuery {
+                id: ts_instance_id.clone().into(),
+                limit: Some(10),
+                ..Default::default()
+            }],
+            start: Some(start.into()),
+            end: Some((start + 10_800_000).into()),
+            ..Default::default()
+        })
+        .await
+        .unwrap();
+    let deleted_states = raw_after_delete
+        .into_iter()
+        .next()
+        .map(|response| response.datapoints.state().unwrap_or_default())
+        .unwrap_or_default();
+    assert!(deleted_states.is_empty());
+
+    let _ = beta_delete_instances(
+        &client,
+        &[
+            NodeOrEdgeSpecification::Node(ItemId {
+                space: space.clone(),
+                external_id: ts_external_id,
+            }),
+            NodeOrEdgeSpecification::Node(ItemId {
+                space,
+                external_id: state_set_external_id,
+            }),
+        ],
+    )
+    .await;
+}
+
+#[tokio::test]
 async fn retrieve_latest() {
     use std::time::{SystemTime, UNIX_EPOCH};
 
@@ -237,7 +469,7 @@ async fn retrieve_latest() {
         .retrieve_latest_datapoints(
             &[LatestDatapointsQuery {
                 before: Some(format!("{}", start + 200_000)),
-                id: Identity::Id { id: ts.id },
+                id: Identity::Id { id: ts.id }.into(),
                 ..Default::default()
             }],
             false,
@@ -337,7 +569,7 @@ async fn create_retrieve_double_datapoints_with_status() {
                 before: Some((start + 10000).to_string()),
                 include_status: Some(true),
                 ignore_bad_data_points: Some(false),
-                id: Identity::from(ts.id),
+                id: Identity::from(ts.id).into(),
                 ..Default::default()
             }],
             false,
